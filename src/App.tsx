6ea86910618811ce90f6c7648e9b15b72ab19ef7
hieu356xz <kotarou2k4@gmail.com>
2024-12-15 06:31:58 +0700

    fix: Loại bỏ react router vì chưa dùng đến

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CurrSubjectProvider from "./contexts/CurrSubjectProvider";
 import CurrTestProvider from "./contexts/CurrTestProvider";
 import ThemeProvider, { ThemeContext } from "./contexts/ThemeProvider";
 import { useContext } from "react";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element={<HomePage />}></Route>)
-);
 
 const App = () => {
   return (
@@ -31,7 +21,7 @@ const AppContent = () => {
 
   return (
     <div className="App" data-theme={theme}>
-      <RouterProvider router={router} />
+      <HomePage />
     </div>
   );
 };
