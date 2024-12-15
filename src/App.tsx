393a6bf9069abb6535f87020eb26ce6a50063a98
hieu356xz <kotarou2k4@gmail.com>
2024-12-15 21:43:56 +0700

    fix: Môn học mặc định được fetch từ App thay vì CurrSubjectSlice

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
 import HomePage from "./pages/HomePage";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "./redux/store";
+import { useEffect } from "react";
+import { setCurrSubject } from "./redux/CurrSubjectSlice";
+import QueryDb from "./data/QueryDb";
+import Subject from "./data/Subject";
 
 const App = () => {
   const theme = useSelector((state: RootState) => state.theme.theme);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchDefaultSubject = async () => {
+      try {
+        const data = await QueryDb(
+          `select *
+            from Subjects 
+            where SemesterID = (select ID 
+                                from Semesters 
+                                where ID = 6)
+            limit 1`
+        );
+
+        const subject = new Subject(JSON.parse(data)[0]);
+        dispatch(setCurrSubject(subject));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        return null;
+      }
+    };
+
+    fetchDefaultSubject();
+  }, []);
 
   return (
     <div className="App" data-theme={theme}>
--- src/redux/CurrSubjectSlice.ts
+++ src/redux/CurrSubjectSlice.ts
@@ -1,28 +1,8 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import Subject from "../data/Subject";
-import QueryDb from "../data/QueryDb";
-
-const fetchDefaultSubject = async () => {
-  try {
-    const data = await QueryDb(
-      `select *
-            from Subjects 
-            where SemesterID = (select ID 
-                                from Semesters 
-                                where ID = 6)
-            limit 1`
-    );
-
-    const subject = new Subject(JSON.parse(data)[0]);
-    return subject;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return null;
-  }
-};
 
 const initialState = {
-  subject: await fetchDefaultSubject(),
+  subject: null as Subject | null,
 };
 
 const CurrSubjectSlice = createSlice({
