db52a3b84ef3c6580ef7d1a41043329e7a4b388e
hieu356xz <kotarou2k4@gmail.com>
2024-12-22 20:42:21 +0700

    beta-feature: Chuyển QueryDb sang singleton pattern

--- src/data/QueryDb.tsx
+++ src/data/QueryDb.tsx
@@ -1,26 +1,33 @@
-import { createDbWorker } from "sql.js-httpvfs";
 import workerUrl from "sql.js-httpvfs/dist/sqlite.worker.js?url";
 import wasmUrl from "sql.js-httpvfs/dist/sql-wasm.wasm?url";
 import dbUrl from "./database.sqlite3?url";
+import { createDbWorker } from "sql.js-httpvfs";
+
+let dbWorker: any = null;
+
+const initializeDbWorker = async () => {
+  if (!dbWorker) {
+    dbWorker = await createDbWorker(
+      [
+        {
+          from: "inline",
+          config: {
+            serverMode: "full",
+            url: dbUrl,
+            requestChunkSize: 4096,
+          },
+        },
+      ],
+      workerUrl.toString(),
+      wasmUrl.toString()
+    );
+  }
+  return dbWorker;
+};
 
 const QueryDb = async (queryString: string) => {
-  const worker = await createDbWorker(
-    [
-      {
-        from: "inline",
-        config: {
-          serverMode: "full",
-          url: dbUrl,
-          requestChunkSize: 4096,
-        },
-      },
-    ],
-    workerUrl.toString(),
-    wasmUrl.toString()
-  );
-
+  const worker = await initializeDbWorker();
   const result = await worker.db.query(queryString);
-
   return JSON.stringify(result);
 };
 
