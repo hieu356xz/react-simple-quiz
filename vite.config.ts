2a5f9d305079d9cf6da94a1efea77161506b1177
hieu356xz <kotarou2k4@gmail.com>
2025-07-12 22:53:29 +0700

    update: Redirect from github pages to new website

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,25 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "github-pages-redirect",
+      transformIndexHtml(html) {
+        const buildMode = process.env.VITE_MODE || "default";
+
+        if (buildMode === "github-pages") {
+          return html.replace(
+            "<title>Ôn tập</title>",
+            `<title>Redirecting to new site...</title>
+    <meta http-equiv="refresh" content="0; URL=https://react-simple-quiz-delta.vercel.app/" />
+    <link rel="canonical" href="https://react-simple-quiz-delta.vercel.app/" />`
+          );
+        }
+        return html;
+      },
+    },
+  ],
   base: "./",
   build: {
     rollupOptions: {
