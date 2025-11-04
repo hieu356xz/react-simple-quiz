3adddd40756d32e7c0867b2b626b692461e11caf
hieu356xz <kotarou2k4@gmail.com>
2025-11-05 01:45:48 +0700

    chore: Update HTMLPaserImageOptions to handle additional serverAws conditions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,7 +5,11 @@ import shuffle from "lodash/shuffle";
 const HTMLPaserImageOptions: HTMLReactParserOptions = {
   replace(domNode) {
     const element = domNode as Element;
-    if (element.attribs && element.attribs["data-org"] === "serverAws") {
+    if (
+      element.attribs &&
+      (element.attribs["data-org"] === "serverAws" ||
+        element.attribs["alt"]?.includes("serverAws"))
+    ) {
       element.attribs[
         "src"
       ] = `https://pczxqazwgbrrqalnwolo.supabase.co/storage/v1/object/public/react-simple-quiz-images/${element.attribs["src"]}.png`;
